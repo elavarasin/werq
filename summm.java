e9a8909c9d1987e694e536b7bd14872e986cd14b
elavarasin <elavarasi1596@gmail.com>
2016-08-08 03:42:08 -0700

    summm.java

---
+++ summm.java
@@ -0,0 +1,22 @@
+public  class summm{
+
+	public static void main(String args[]){
+	int n=Integer.parseInt(args[0]);
+	int x=0;
+	int sum=0;
+	int y=x;
+	
+	while(n>0)
+
+	{
+	x=n%10;
+	sum=sum+(int)Math.pow(x,y);
+	n=n/10;
+	y=b;
+	
+
+	}
+	System.out.println(sum);
+	}
+}
+	
